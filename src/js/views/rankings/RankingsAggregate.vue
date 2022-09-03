d1bfc59f163b049c3518c44f3cd284f6917eb4d0
Eli Wilson <etwilson@med.umich.edu>
2022-09-03 12:46:41 -0400

    Add test call to backend for Heroku testing.

--- src/js/views/rankings/RankingsAggregate.vue
+++ src/js/views/rankings/RankingsAggregate.vue
@@ -1,10 +1,11 @@
 <script setup>
     import { request, gql } from 'graphql-request'
     import { ref } from 'vue'
+    import axios from 'axios'
     import AggregateTable from "./components/AggregateTable.vue"
 
     const data = ref()
-    let tableIsShowing = ref(true)
+    let showTable = ref(false)
 
     function buildQuery(code) {
         const query = gql`
@@ -19,12 +20,19 @@
     }
 
     function onSubmit(event) {
+        console.log(import.meta.env.VITE_API_URL)
+        axios.get(import.meta.env.VITE_API_URL)
+            .then(function (response) {
+                // handle success
+                console.log(response.data);
+            })
         // ToDo: Validate form, show errors, all that stuff
 
         // Split the report codes by new line
         let codes = event.target.elements.reportCodes.value.split("\n")
         // Loop through the codes and get the data
-        codes.forEach(x => {
+        codes.forEach(code => {
+            //request('https://api.spacex.land/graphql/', buildQuery(code)).then((data) => console.log(data))
             // Loop through all the encounters
 
             // ToDo: Handle exclusions
@@ -33,7 +41,7 @@
 
             // If character is in the array already, then append the ranking number and update the entry
 
-            tableIsShowing = true;
+            showTable = true;
         })
     }
 
@@ -44,7 +52,7 @@
 </script>
 
 <template>
-    <div v-if="!tableIsShowing" class="row justify-content-center">
+    <div v-if="!showTable" class="row justify-content-center">
         <form class="col-4" @submit.prevent="onSubmit">
         
             <div class="collapse show card card-body row justify-content-center">
@@ -53,10 +61,10 @@
             </div>
         </form>
     </div>
-    <div v-if="tableIsShowing" class="row justify-content-center">
+    <div v-if="showTable" class="row justify-content-center">
         
         <div class="col-4">
-            <button @click="function() {tableIsShowing = false }" type="button" class="btn btn-custom mb-2">Reset</button>   
+            <button @click="function() {showTable = false }" type="button" class="btn btn-custom mb-2">Reset</button>   
             <AggregateTable />
         </div>        
     </div>
